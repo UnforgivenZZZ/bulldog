3b141f4f2f6b78ca2839c72c54432a7bcb86d88f
Jialiang Zhou <ericcareer@hotmail.com>
2019-03-16 20:58:17 -0700

    Update users.java

--- bulldog/src/com/model/users.java
+++ bulldog/src/com/model/users.java
@@ -1,5 +1,19 @@
 package com.model;
 
 public class users {
-
+  
+  @Setter
+  @Getter
+  String userName;
+  @Setter
+  @Getter
+  String pass;
+  @Setter
+  @Getter
+  int score;
+  
+  public users(){
+  }
+  public users(String name, String pass){
+  }
 }
